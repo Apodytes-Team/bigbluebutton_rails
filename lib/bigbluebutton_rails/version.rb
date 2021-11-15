8ea29dec88355ec8c63469d6d9bdcf1f35ae5583
Damilola Omiyera <damilolaomiyera@yahoo.com>
2021-11-15 14:31:19 +0100

    version change

--- lib/bigbluebutton_rails/version.rb
+++ lib/bigbluebutton_rails/version.rb
@@ -1,7 +1,3 @@
 module BigbluebuttonRails
-<<<<<<< HEAD
-  VERSION = "1.3.0".freeze
-=======
-  VERSION = "1.3.0.mweb1".freeze
->>>>>>> 6a0b6b4f54f58623274d51601dca6da340add1b8
+  VERSION = "1.3.1".freeze
 end
